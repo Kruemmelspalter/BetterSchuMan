02edcf0c52a380e0d3c664526832c6314ae0d201
Kruemmelspalter <mtpmtp299@gmail.com>
2021-07-20 16:36:25 +0200

    Removed redirect because static app is in root

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,6 @@ import express from 'express'
 import * as ApiRouter from './routes/api.js'
 const app = express()
 
-app.get('/', (req, res) => {
-    res.redirect('/app/index.html')
-})
-
 app.use(express.static('./static'))
 app.use('/api', ApiRouter.router)
 
