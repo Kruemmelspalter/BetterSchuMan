3f488150c9d4594dfe190d9a3bebbebcd59cdda1
Kruemmelspalter <mtpmtp299@gmail.com>
2021-08-30 12:16:26 +0200

    Transfer Angular base code to Vue

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,21 @@
 import express from 'express'
 import * as ApiRouter from './routes/api.js'
+import expressWinston from 'express-winston'
+import winston from "winston";
+
 const app = express()
+
+app.use(expressWinston.logger({
+    transports: [
+        new winston.transports.Console()
+    ],
+    meta: false,
+    msg: "{{req.method}} {{req.url}}",
+}))
+
 
 app.use(express.static('./static'))
 
 app.use('/api', ApiRouter.router)
 
-app.listen(8080)
+app.listen(80, () => console.log(`Listening`))
