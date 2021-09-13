e5549da0f90487d2cada6862086c767565ace97f
Kruemmelspalter <mtpmtp299@gmail.com>
2021-09-13 14:07:51 +0200

    Fixed conflicts

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ FROM node:lts
 WORKDIR /opt/betterschuman
 
 COPY backend/package.json .
-RUN npm install
+RUN npm install --production
 
 COPY backend .
 
--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -2,3 +2,9 @@ import {Router} from "express";
 
 export const router = Router()
 
+router.post('/', (req, res) => {
+    res
+        //.header('X-New-Bearer-Token', 'a')
+        .sendStatus(200)
+        .send()
+}) // TODO Only for debug
