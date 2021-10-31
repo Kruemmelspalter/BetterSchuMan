05e389f018dbe82988b70bab52e5e0aee17a55ef
Kruemmelspalter <mtpmtp299@gmail.com>
2021-10-31 15:52:57 +0100

    Remove debug stuff

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as morgan from 'morgan';
-import { join } from 'path';
 
 async function bootstrap() {
-  console.log(join(__dirname, 'static'));
   const app = await NestFactory.create(AppModule);
 
   app.use(
