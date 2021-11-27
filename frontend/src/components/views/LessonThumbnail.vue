9bca1d8b6b5e77bd94d2150a8cafc99a9b406953
Kruemmelspalter <mtpmtp299@gmail.com>
2021-11-27 07:26:16 +0100

    add frontend support for cancelled lessons

--- frontend/src/components/views/LessonThumbnail.vue
+++ frontend/src/components/views/LessonThumbnail.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container">
+  <div :class="lesson.cancelled ? 'cancelled' : ''" class="container">
     <div :title="lesson.subject.name" class="name">
       {{ lesson.subject.abbreviation }}
     </div>
@@ -31,6 +31,16 @@ export default {
   font-size: 1.5vh;
 }
 
+.cancelled {
+  background-color: var(--color-text-accent-2);
+}
+
+.cancelled > div {
+  color: var(--color-text-accent-1);
+  background-color: var(--color-text-accent-2);
+}
+
+
 .name {
   font-weight: bold;
 }
@@ -38,4 +48,5 @@ export default {
 .room {
   margin: auto;
 }
+
 </style>
