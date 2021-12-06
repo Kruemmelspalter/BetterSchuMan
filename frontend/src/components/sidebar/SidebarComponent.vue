57447ffbbc4d9c663e74f4fdf74aa9f1a14bdebf
Kruemmelspalter <mtpmtp299@gmail.com>
2021-12-06 16:54:45 +0100

    overflow scroll sidebar

--- frontend/src/components/sidebar/SidebarComponent.vue
+++ frontend/src/components/sidebar/SidebarComponent.vue
@@ -7,10 +7,12 @@
       <button class="tabbutton material-icons" @click="activate(3)">chat</button>
       <button class="tabbutton material-icons" @click="activate(-1)">close</button>
     </div>
-    <schedule-component v-show="activeTab === 0" class="tabcontent" />
-    <calendar-component v-show="activeTab === 1" class="tabcontent" />
-    <notifications-component v-show="activeTab === 2" class="tabcontent" />
-    <chat-component v-show="activeTab === 3" class="tabcontent" />
+    <div id="sidebarcontent">
+      <schedule-component v-show="activeTab === 0" class="tabcontent" />
+      <calendar-component v-show="activeTab === 1" class="tabcontent" />
+      <notifications-component v-show="activeTab === 2" class="tabcontent" />
+      <chat-component v-show="activeTab === 3" class="tabcontent" />
+    </div>
   </div>
 </template>
 
@@ -37,5 +39,8 @@ export default {
 </script>
 
 <style scoped>
-
+#sidebarcontent {
+  overflow-y: scroll;
+  max-height: 87.5vh;
+}
 </style>
