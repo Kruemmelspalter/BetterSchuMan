68acb3e03778a85a81704d36f653128c70511814
Kruemmelspalter <mtpmtp299@gmail.com>
2021-12-07 18:28:38 +0100

    fix logger fixes #33

--- backend/src/logging/logger.interceptor.ts
+++ backend/src/logging/logger.interceptor.ts
@@ -7,24 +7,25 @@ import {
 } from '@nestjs/common';
 import { Observable, tap } from 'rxjs';
 import * as uuid from 'uuid';
+import { DateTime } from 'luxon';
 
 @Injectable()
 export class LoggerInterceptor implements NestInterceptor {
   private readonly logger = new Logger('HTTP');
+
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    const begin = new Date();
+    const begin = DateTime.now();
     const id = uuid
       .v1({
-        msecs: Math.floor(begin.getTime() / 1000),
-        nsecs: begin.getTime() % 1000,
+        msecs: Math.floor(begin.toMillis()),
       })
       .replaceAll('-', '');
-    const request = context.switchToHttp().getRequest() as Request;
+    const request = context.switchToHttp().getRequest();
 
     request.headers['X-BetterSchuMan-ID'] = id;
     this.logger.log({
       id: id,
-      time: begin.getTime(),
+      time: begin.toMillis(),
       type: 'request',
       path: request.url,
     });
@@ -33,10 +34,9 @@ export class LoggerInterceptor implements NestInterceptor {
 
     return next.handle().pipe(
       tap((_) => {
-        const end = new Date();
         this.logger.log({
           id: id,
-          time: end.getTime(),
+          time: DateTime.now().toMillis(),
           type: 'response',
           status: statusCode,
           path: request.url,
