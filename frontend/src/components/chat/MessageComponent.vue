060ff003efb2b7617c9ad5d0dfccd3d57b9e8bd0
Kruemmelspalter <mtpmtp299@gmail.com>
2022-02-04 19:18:16 +0100

    show and download attachments

--- frontend/src/components/chat/MessageComponent.vue
+++ frontend/src/components/chat/MessageComponent.vue
@@ -2,12 +2,18 @@
   <div class="message">
     {{ message.sender.firstname }} {{ message.sender.lastname }}
     <span class="material-icons subtext">navigate_next</span>
-    <span class="message" v-html="formattedMessage"/>
+    <span class="message" v-html="formattedMessage" />
+    <div v-if="message.attachments.length !== 0">
+      <a v-for="a in message.attachments" :key="a.url" :href="a.url" download>
+        <span class="material-icons">description</span>
+        {{ a.filename }}<br />
+      </a>
+    </div>
   </div>
 </template>
 
 <script>
-import linkifyHtml from 'linkifyjs/lib/linkify-html';
+import linkifyStr from 'linkifyjs/lib/linkify-string';
 
 export default {
   name: 'MessageComponent',
@@ -20,7 +26,7 @@ export default {
     formattedMessage() {
       let message = this.message.text;
       message = new Option(message).innerHTML;
-      message = linkifyHtml(message);
+      message = linkifyStr(message, { target: '_blank' });
       message = message.replaceAll(/\*(\w+)\*/g, '<b>$1</b>');
       return message;
     },
